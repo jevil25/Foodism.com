8bf0c5cc18a22e04f99d73740fffd613488b0c0a
AaronNazareth <4nm20cs005@nmamit.in>
2022-11-12 11:36:19 +0530

    Added new theme and logo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const getElement = (selector) => {
   const element = document.querySelector(selector)
-
+  
   if (element) return element
   throw Error(
     `Please double check your class names, there is no ${selector} class`
