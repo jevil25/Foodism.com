222b7fbcc3a3ff8e47342cb03a0f08487586d08a
AaronNazareth <4nm20cs005@nmamit.in>
2022-12-07 21:43:08 +0530

    New queries added

--- server.js
+++ server.js
@@ -51,8 +51,7 @@ const recipesSchema=new mongoose.Schema({
     },
     continent_id:{
         type:Number,
-        required:true,
-        unique:true
+        required:true
     },
     preptime:{
         type:Number,
@@ -114,11 +113,63 @@ const nutritionSchema=new mongoose.Schema({
 const Continents = new mongoose.model("Continents", foodcountrySchema);
 const Recipe=new mongoose.model("Recipes",recipesSchema);
 const Tags=new mongoose.model("Tags",foodtagSchema);
-const nutrition=new mongoose.model("Nutrition",nutritionSchema);
+const Nutrition=new mongoose.model("Nutrition",nutritionSchema);
 
 
 
-module.exports={Continents,Recipe,Tags,nutrition}; //sends data to database
+module.exports={Continents,Recipe,Tags,Nutrition}; //sends data to database
+
+const nut=new Nutrition({
+  nutrition_id:3,
+  calories:240,
+  fat:17,
+  carbs:0,
+  protein:21
+})
+
+// nut.save();
+
+const load=new Recipe({
+recipe_name:'Seared Burgers With Easy-Melting Comté Cheese',
+recipe_pic:'../assets/recipes/steamed-cheese-burger.webp',
+recipe_description:"Some folks balk at the idea of putting American cheese anywhere near their burger. Cheese? You call that stuff cheese? More like orange chemical plasti-crap. No thanks! they say, as they peel off the orange slices and fling them against the wall all angry-like",
+recipe_id:3,
+tag_id:[],
+continent_id:2,
+preptime:15,
+cooktime:15,
+servings:2,
+recipe_ingredients:["2 ounces coarsely grated aged Comté cheese",
+"1 teaspoon instant flour (like Wondra)",
+"2 teaspoons heavy cream or milk",
+"1/4 teaspoon vegetable oil",
+"8 ounces of freshly ground beef, formed into two 4-ounce balls",
+"Kosher salt and freshly ground black pepper",
+"2 toasted hamburger buns",
+"Toppings and condiments as desired"],
+recipe_steps:["Combine cheese, flour, and milk in a small bowl and toss with a spoon to combine. Divide mixture evenly between two small ramekins and set aside. Set a steamer insert into a large saucepot with 1 inch of water. Bring water to a simmer over medium-high heat and cover pot.","Heat vegetable oil in a 10-inch cast iron or stainless steel skillet over high heat until smoking. Add beef balls to skillet and smash firmly with a stiff spatula into 4- to 4 1/2-inch patties. Season generously with salt and pepper.","While patties cook on first side, transfer cheese cups to steamer and close lid. After the first side is well browned (about 1 1/2 minutes), carefully scrape patties up from bottom us skillet using a stiff spatula and flip. Season top side with salt and pepper. Continue to cook until second side is browned, about 1 minute longer.","Transfer patties to toasted bun bottoms. Carefully pick up a ramekin full of cheese with a pot holder and stir with a spoon until homogenous and completely smooth. Scrape cheese out of ramekin over burger. Repeat with second burger. Serve immediately with condiments and toppings as desired."],
+nutrition_id:3
+})
+
+// const tag=new Tags({
+// tag_id:5,
+// tag_name:"Ice cream"
+// })
+
+// const tag2=new Tags({
+//   tag_id:6,
+//   tag_name:"Sweet"
+//   })
+
+  // const tag3=new Tags({
+  //   tag_id:7,
+  //   tag_name:"Deserts"
+  //   })
+
+  // load.save();
+  // tag.save();
+  // tag2.save();
+  // tag3.save();s
 
 
 const app=express();
@@ -145,16 +196,41 @@ app.post("/index",function(req,res){
 app.post("/recipes",async function(req,res){
     app.set('view engine','hbs');
     console.log(req.body.continent);
-    let recipes= await Recipe.aggregate([
-        {
-            '$lookup': {
-                'form': 'continents',
-                'localField':'continent_id',
-                'foreignField': 'continent_id',
-                'as': 'continentDetails'
-            }
+    let recipes= await Continents.aggregate([
+      {
+        '$match': {
+          'continent_name': req.body.continent
         }
-    ])
+      }, {
+        '$lookup': {
+          'from': 'recipes', 
+          'localField': 'continent_id', 
+          'foreignField': 'continent_id', 
+          'as': 'Recipe'
+        }
+      }, {
+        '$addFields': {
+          'recipes': {
+            '$arrayElemAt': [
+              '$Recipe', 0
+            ]
+          }
+        }
+      }, {
+        '$addFields': {
+          'recipe_name': '$recipes.recipe_name', 
+          'preptime': '$recipes.preptime', 
+          'cooktime': '$recipes.cooktime', 
+          'recipe_pic_link': '$recipes.recipe_pic'
+        }
+      }, {
+        '$unset': [
+          'continent_id', 'continent_name', 'Recipe', 'recipes', '_id'
+        ]
+      }
+    ]);
+    console.log(recipes)
+    res.render("recipes.hbs",{recipe:recipes})
 })
 
 
